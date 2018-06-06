78ef6bdde16e53fb0a3bc009de4ead710b5f16e2
gdpinchina <gdpinchina@126.com>
2018-06-06 12:05:15 +0800

    To support DeeperGTAV
    Modify the client.py to support DeeperGTAV:
    1. Data of each trip is recorded in a file. Namely: trip0.pz, trip1.pz, ...;
    2. Add support for lidar data;
    3. Add support for driving modes;

--- deepgtav/client.py
+++ deepgtav/client.py
@@ -5,30 +5,57 @@ import json
 import socket, struct
 import pickle
 import gzip
+import os
+import numpy as np
 
 class Targets:
-    def __init__(self, datasetPath, compressionLevel):
+    def __init__(self, datasetPath, compressionLevel, divideByTrip):
         self.pickleFile = None
-        
-        if datasetPath != None:
-            self.pickleFile = gzip.open(datasetPath, mode='ab', compresslevel=compressionLevel)
+        self.datasetPath = datasetPath
+        self.compressionLevel = compressionLevel
+        self.divideByTrip = divideByTrip
+        self.carNum = 0
 
-    def parse(self, frame, jsonstr):
+        if not self.divideByTrip and self.datasetPath != None:
+            fullpath = os.path.dirname(self.datasetPath) + '\\' + 'dataset.pz'
+            self.pickleFile = gzip.open(fullpath, mode='ab', compresslevel=self.compressionLevel)
+        elif self.datasetPath != None:
+            while True:
+                if os.path.exists(os.path.dirname(self.datasetPath) + '\\' + 'trip' + str(self.carNum) + '.pz'): self.carNum += 1
+                else: break
+
+    def parse(self, frame, lidar, jsonstr):
         try:
             dct = json.loads(jsonstr)
         except ValueError:
             return None
-        
-        dct['frame'] = frame
+
+        if self.divideByTrip and self.datasetPath != None:
+            assert('drivingMode' in dct)
+            if dct['drivingMode'][0] == 0 :
+                fullpath = os.path.dirname(self.datasetPath) + '\\' + 'trip' + str(self.carNum) + '.pz'
+                if self.pickleFile != None: self.pickleFile.close()  
+                self.pickleFile = gzip.open(fullpath, mode='ab', compresslevel=self.compressionLevel)
+                self.carNum += 1
+                
+            
+        if frame != None: dct['frame'] = frame
+        if lidar != None: dct['lidar'] = np.frombuffer(lidar, dtype=np.float32)
         if self.pickleFile != None:
             pickle.dump(dct, self.pickleFile)
         return dct
 
+    def close(self):
+        if self.pickleFile != None:
+            self.pickleFile.close()  
+
 class Client:
-    def __init__(self, ip='localhost', port=8000, datasetPath=None, compressionLevel=0):
+    def __init__(self, ip='localhost', port=8000, datasetPath=None, compressionLevel=0, divideByTrip=False):
         print('Trying to connect to DeepGTAV')
         
-        self.targets = Targets(datasetPath, compressionLevel)
+        self.targets = Targets(datasetPath, compressionLevel, divideByTrip)
+        self.frame, self.lidar = False, False
+        self.check = np.array([0, 0])
 
         try:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,6 +66,9 @@ class Client:
             print('Successfully connected to DeepGTAV')
 
     def sendMessage(self, message):
+        if str(message.__class__.__name__) == 'Start' or str(message.__class__.__name__) == 'Config':
+            self.frame, self.lidar = message.activate_bytes_frame()
+
         jsonstr = message.to_json().encode('utf-8')
         try:
             self.s.sendall(len(jsonstr).to_bytes(4, byteorder='little'))
@@ -49,18 +79,31 @@ class Client:
         return True
 
     def recvMessage(self):
-        frame = self._recvall()
-        if not frame: 
-            print('ERROR: Failed to receive frame')     
-            return None
+
+        if self.frame:
+            frame = self._recvall()
+            if not frame: 
+                print('ERROR: Failed to receive frame')     
+                return None
+        else: frame = None
+
+        if self.lidar:
+            lidar = self._recvall()
+            if not lidar: 
+                print('ERROR: Failed to receive lidar')     
+                return None
+        else: lidar = None
+
         data = self._recvall()
         if not data: 
             print('ERROR: Failed to receive message')       
             return None
-        return self.targets.parse(frame, data.decode('utf-8'))
 
+        return self.targets.parse(frame, lidar, data.decode('utf-8'))
+
+            
     def _recvall(self):
-        #Receive first size of message in bytes
+        #Receive first size of message in bytes, make up for 4 bytes then read
         data = b""
         while len(data) < 4:
             packet = self.s.recv(4 - len(data))
@@ -78,3 +121,5 @@ class Client:
 
     def close(self):
         self.s.close()
+        self.targets.close()
+
