7394f5964a99bd68b2c1f821af414e25ea0f4405
gdpinchina <gdpinchina@126.com>
2018-06-06 12:06:37 +0800

    To support DeeperGTAV
    Modify the message.py to support DeeperGTAV:
    1. Data of each trip is recorded in a file. Namely: trip0.pz, trip1.pz, ...;
    2. Add support for lidar data;
    3. Add support for driving modes;

--- deepgtav/messages.py
+++ deepgtav/messages.py
@@ -6,16 +6,19 @@ import numpy as np
 from numpy.lib.stride_tricks import as_strided
 
 class Scenario:
-    def __init__(self, location=None, time=None, weather=None, vehicle=None, drivingMode=None):
+    def __init__(self, location=None, time=None, weather=None, vehicle=None, drivingMode=None, route=None, surroundDrivingMode=None):
         self.location = location #[x,y]
         self.time = time #[hour, minute]
         self.weather = weather #string
         self.vehicle = vehicle #string
-        self.drivingMode = drivingMode #[drivingMode, setSpeed]
+        self.drivingMode = drivingMode 
+        self.route = route
+        self.surroundDrivingMode = surroundDrivingMode
 
 class Dataset:
-    def __init__(self, rate=None, frame=None, vehicles=None, peds=None, trafficSigns=None, direction=None, reward=None, 
-            throttle=None, brake=None, steering=None, speed=None, yawRate=None, drivingMode=None, location=None, time=None):
+    def __init__(self, rate=None, frame=None, vehicles=None, peds=None, trafficSigns=None, direction=None, 
+                reward=None, throttle=None, brake=None, steering=None, speed=None, yaw=None, yawRate=None, drivingModeMsg=None, 
+                location=None, time=None, rageMatrices=None, cameraInfo=None, eulerAngles=None, lidar=None, isCollide=None, acceleration=None):
         self.rate = rate #Hz
         self.frame = frame #[width, height]
         self.vehicles = vehicles #boolean
@@ -27,10 +30,17 @@ class Dataset:
         self.brake = brake #boolean
         self.steering = steering #boolean
         self.speed = speed #boolean
+        self.yaw = yaw #boolean
         self.yawRate = yawRate #boolean
-        self.drivingMode = drivingMode #boolean
+        self.drivingModeMsg = drivingModeMsg #boolean
         self.location = location #boolean
         self.time = time #boolean
+        self.rageMatrices = rageMatrices #boolean
+        self.cameraInfo = cameraInfo #boolean
+        self.eulerAngles = eulerAngles #boolean
+        self.lidar = lidar #boolean
+        self.isCollide = isCollide #boolean
+        self.acceleration = acceleration #boolean
 
 class Start:
     def __init__(self, scenario=None, dataset=None):
@@ -45,10 +55,14 @@ class Start:
             _scenario = self.scenario.__dict__
 
         if (self.dataset != None):
-            _dataset = self.dataset.__dict__            
+            _dataset = self.dataset.__dict__     
 
         return json.dumps({'start':{'scenario': _scenario, 'dataset': _dataset}})
 
+    def activate_bytes_frame(self):
+        return True if self.dataset.frame != None else False, True if self.dataset.lidar != None else False
+
+        
 
 class Config:
     def __init__(self, scenario=None, dataset=None):
@@ -63,9 +77,14 @@ class Config:
             _scenario = self.scenario.__dict__
 
         if (self.dataset != None):
-            _dataset = self.dataset.__dict__            
+            _dataset = self.dataset.__dict__         
 
         return json.dumps({'config':{'scenario': _scenario, 'dataset': _dataset}})
+
+    def activate_bytes_frame(self):
+        return True if self.dataset.frame != None else False, True if self.dataset.lidar != None else False
+
+        
 
 class Stop:
     def to_json(self):
@@ -86,4 +105,6 @@ def frame2numpy(frame, frameSize):
     strideWidth = int((frameSize[0] * 3 + 3) / 4) * 4
     # Return a copy because custom strides are not supported by OpenCV.
     return as_strided(buff, strides=(strideWidth, 3, 1), shape=(frameSize[1], frameSize[0], 3)).copy()
+
+
 
